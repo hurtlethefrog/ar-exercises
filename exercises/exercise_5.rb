4542a370dd3556a64720afadae0c4f9524e79b68
Duncan Haran <duncan.haran@gmail.com>
2019-08-19 23:58:46 +0000

    querying stores with math params

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -7,4 +7,9 @@ require_relative './exercise_4'
 puts "Exercise 5"
 puts "----------"
 
-# Your code goes here ...
+@total_revenue = Store.sum("annual_revenue")
+
+@average_revenue = Store.average("annual_revenue")
+
+@number_of_sucessful_stores = Store.where("annual_revenue >= :min",
+  {min: [1000000]}).count()
