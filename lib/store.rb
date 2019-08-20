03cc9b15767a1b78535a1ab06ba78c579f8a8e22
Duncan Haran <duncan.haran@gmail.com>
2019-08-20 17:34:26 +0000

    removed custom validator

--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,13 @@
 class Store < ActiveRecord::Base
+  has_many :employees
+  validates :name, length: {minimum: 3}
+  validates :annual_revenue, numericality: {geater_than_or_equal: 0}
 
+  # validate :must_sell_something
+
+  # def must_sell_something
+  #   if !mens_apparel.present? || !womens_apparel.present?
+  #     errors.add(:mens_apparel, "Must sell something!")
+  #   end
+  # end
 end
